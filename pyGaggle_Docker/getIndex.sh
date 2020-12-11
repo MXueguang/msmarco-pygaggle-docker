e440da29fcbd414ed70b9d4fb43533e8d36a36d6
qguo96 <guogai1996@gmail.com>
2020-12-10 20:13:34 -0500

    remove index file

--- pyGaggle_Docker/getIndex.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget https://git.uwaterloo.ca/jimmylin/anserini-indexes/raw/master/index-robust04-20191213.tar.gz
-tar xvfz index-robust04-20191213.tar.gz -C pygaggle/indexes
-rm index-robust04-20191213.tar.gz
